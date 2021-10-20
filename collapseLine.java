221a218686e70d8d5d5d430aacab5bfacaf97a35
leningy <52613760+leningy@users.noreply.github.com>
2021-10-19 22:22:33 -0400

    Create collapseLine.java

---
+++ collapseLine.java
@@ -0,0 +1,32 @@
+    /**
+     * Returns string with spaces collapsed
+     *
+     * @param line input string to collapse spaces
+     * @return string with spaces collapsed
+     */
+    public static String collapseLine(String line) {
+        String s = "";
+        int i;
+
+        for (i = 0; i < line.length(); i++) {
+            /*if (i == 0 || i == line.length() - 1) {
+                if (!Character.isWhitespace(line.charAt(i))) {
+                    s += line.charAt(i);
+                }
+                continue;
+            }*/
+            if (i == 0) { //does not work and creates a lot of errors
+                while (Character.isWhitespace(line.charAt(i))) {
+                    i++;
+                }
+                continue;
+            }
+            if (!Character.isWhitespace(line.charAt(i))) {
+                s += line.charAt(i);
+            }
+            else if (Character.isWhitespace(line.charAt(i)) && !Character.isWhitespace(line.charAt(i + 1))) {
+                s += line.charAt(i);
+            }
+        }
+        return s;
+    }
