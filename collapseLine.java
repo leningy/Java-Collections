5706168d9b10aec159a015a36c14012e1f8a89d2
leningy <52613760+leningy@users.noreply.github.com>
2021-10-20 20:33:11 -0400

    Update collapseLine.java

--- collapseLine.java
+++ collapseLine.java
@@ -9,23 +9,35 @@
         int i;
 
         for (i = 0; i < line.length(); i++) {
-            /*if (i == 0 || i == line.length() - 1) {
+            if (/*i == 0 || */ i == line.length() - 1) {
                 if (!Character.isWhitespace(line.charAt(i))) {
                     s += line.charAt(i);
                 }
+                else { // works for head spaces but produces two spaces after the first word
+                    if (i == 0) {
+                        while (Character.isWhitespace(line.charAt(i))) {
+                            i++;
+                            s += "";
+                      }
+                      i--;
+                    }
+                }
                 continue;
-            }*/
-            if (i == 0) { //does not work and creates a lot of errors
+            }
+            /* if (i == 0) { //does not work and creates a lot of errors
                 while (Character.isWhitespace(line.charAt(i))) {
                     i++;
                 }
                 continue;
-            }
+            }*/
             if (!Character.isWhitespace(line.charAt(i))) {
                 s += line.charAt(i);
             }
             else if (Character.isWhitespace(line.charAt(i)) && !Character.isWhitespace(line.charAt(i + 1))) {
                 s += line.charAt(i);
+            }
+            if (s.length() > 1 && Character.isWhitespace(s.charAt(0))) {
+            s = s.substring(1);
             }
         }
         return s;
