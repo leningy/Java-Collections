0338956af948a5c7773824ce2a57418a0616782f
leningy <52613760+leningy@users.noreply.github.com>
2021-10-28 16:59:15 -0400

    Create booleanEqualsObject.java

---
+++ booleanEqualsObject.java
@@ -0,0 +1,12 @@
+/** Condition of the method - being inside the RationalNumber class which has 
+ * 1. private variable numerator, denominator
+ * 2. getNumerator() & getDenominator() to access the private variables
+ */
+
+public boolean equals(Object other) {
+        if (other instanceof RationalNumber) {
+            RationalNumber r = (RationalNumber) other;
+            return numerator == r.getNumerator() && denominator == r.getDenominator();
+        }
+        return false;
+    }
