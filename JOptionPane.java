fc94228b93de335482b028878afbcb9d6c463859
leningy <52613760+leningy@users.noreply.github.com>
2021-11-02 13:48:50 -0400

    Create JOptionPane.java

---
+++ JOptionPane.java
@@ -0,0 +1,21 @@
+/** showMessageDialog(parent, message) */
+
+import javax.swing.*;
+
+/**
+ * Simple example that uses JOptionPane showMessageDialog method.
+ * 
+ * @author Reges and Stepp
+ */
+public class MessageDialogExample {
+    /**
+     * Start program
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "How's the weather?");
+        JOptionPane.showMessageDialog(null, "Second message");
+    }
+}
+
