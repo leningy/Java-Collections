e2093eaa952987e6c3b1b395c187677612f4eb53
leningy <52613760+leningy@users.noreply.github.com>
2021-11-02 13:50:28 -0400

    Update JOptionPane.java

--- JOptionPane.java
+++ JOptionPane.java
@@ -43,3 +43,25 @@ public class ConfirmDialogExample {
         }
     }
 }
+
+//////////////////////////////////////////////////////
+/** showInputDialog(parent, message) */
+import javax.swing.*;
+
+/**
+ * Simple example that uses JOptionPane showInputDialog method.
+ * 
+ * @author Reges and Stepp
+ */
+public class InputDialogExample {
+    /**
+     * Start program
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog(null, "What's yer name, pardner?");
+        JOptionPane.showMessageDialog(null, "Yeehaw, " + name);
+    }
+}
+
