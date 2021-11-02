a5e2c3acd353683534c4c8a5e89e06cddfcfaafb
leningy <52613760+leningy@users.noreply.github.com>
2021-11-02 13:49:37 -0400

    Update JOptionPane.java

--- JOptionPane.java
+++ JOptionPane.java
@@ -19,3 +19,27 @@ public class MessageDialogExample {
     }
 }
 
+/////////////////////////////////////////////////////
+/** showConfirmDialog(parent, message) */
+import javax.swing.*;
+
+/**
+ * Simple example that uses JOptionPane showConfirmDialog method.
+ * 
+ * @author Reges and Stepp
+ */
+public class ConfirmDialogExample {
+    /**
+     * Start program
+     * 
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        int choice = JOptionPane.showConfirmDialog(null, "Erase your hard disk?");
+        if (choice == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "Disk erased!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Cancelled.");
+        }
+    }
+}
